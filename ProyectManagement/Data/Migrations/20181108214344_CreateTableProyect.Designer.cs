23f84817ee1ef9f46f7bbed31135b3eaddd41cca
Juan david <juandavidlagareas@gmail.com>
2018-11-08 16:47:40 -0500

    agrego el modelo proyect

--- ProyectManagement/Data/Migrations/20181108214344_CreateTableProyect.Designer.cs
+++ ProyectManagement/Data/Migrations/20181108214344_CreateTableProyect.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ProyectManagement.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181108214344_CreateTableProyect")]
+    partial class CreateTableProyect
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProyectManagement/Models/Proyect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectManagement.Models
+{
+    public class Proyect
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public EnumProyectManagment Status { get; set; }
+    }
+
+    public enum EnumProyectManagment  
+    {
+        
+        InProcess,
+        Finalized
+    }
+}
