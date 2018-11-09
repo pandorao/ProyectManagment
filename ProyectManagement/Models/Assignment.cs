555537b421286a3a93a797da6299341dedfb7054
ehenry98 <ehenry@uninorte.edu.co>
2018-11-09 12:17:03 -0500

    agrego assignment

---
+++ ProyectManagement/Models/Assignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectManagement.Models
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public int jobId { get; set; }
+        public Job job { get; set; }
+        public int ContributorId { get; set; }
+        public Contributor contributor { get; set; }
+    }
+}
