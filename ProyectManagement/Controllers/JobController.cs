30144eec0243bf93db0b6a6bffe35e0351c16ed8
Juan david <juandavidlagareas@gmail.com>
2018-11-09 17:26:52 -0500

    Se pone como Autorize los controladores del negocio

--- ProyectManagement/Controllers/JobController.cs
+++ ProyectManagement/Controllers/JobController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using ProyectManagement.Models;
 
 namespace ProyectManagement.Controllers
 {
+    [Authorize]
     public class JobController : Controller
     {
         private readonly ApplicationDbContext _context;
