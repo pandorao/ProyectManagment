67a2ba51a4283e7881d354fdd4a356f7bef45cf8
Juan david <juandavidlagareas@gmail.com>
2018-11-11 12:02:55 -0500

    Se edita Assignmet

---
+++ ProyectManagement/Models/AssignmentViewModels/AssignmentCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectManagement.Models.AssignmentViewModels
+{
+    public class AssignmentCreateViewModel
+    {
+        public int ContributorId { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
